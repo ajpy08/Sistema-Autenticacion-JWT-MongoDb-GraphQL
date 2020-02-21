aee0cdbe1caf8912929736e1232450c3c7373b14
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-21 12:23:51 -0600

    Se modifico el schema para agregar el query de login, registro etc. y mutation. Tambien se agrego en query.ts

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,15 +2,6 @@ import { IResolvers } from "graphql-tools";
 
 const query : IResolvers = {
     Query : {
-        hola(): string{
-            return 'Hola Mundoddddd!';
-        },
-        holaConNombre(__: void, { nombre } ): string {
-            return `Hola Mundo ${nombre}`;
-        },
-        holaAlCursoGraphQL(): string {
-            return 'Hola Mundo en el curso de GraphQL';
-        }
     }
 }
 
