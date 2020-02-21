a148c28788c36677a4b1858884b381b5cae57021
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-21 13:36:36 -0600

    creamos el mutation register

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,7 +1,22 @@
+import { Datetime } from './../lib/datetime';
 import { IResolvers } from "graphql-tools";
 
 const mutation : IResolvers = {
-    Mutation : {        
+    Mutation : {   
+        async register(_: void, { user }, { db }) : Promise<any> {
+            const lastUser = await db.collection('users').find()
+            .limit(1).sort({ registerDate: -1 }).toArray();
+
+            if (lastUser.length === 0) {
+                user.id = 1;
+            } else {
+                user.id = lastUser[0].id + 1;
+            }
+            user.registerDate = new Datetime().getCurrentDateTime();
+            await db.collection('users').insertOne(user);
+            
+            return user;
+        }     
     }
 }
 
