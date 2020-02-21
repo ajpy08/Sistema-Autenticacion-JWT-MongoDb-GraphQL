ce92f489eb76cb5b98bd1ad0dab7efbfe6a60e2c
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-21 12:16:05 -0600

    Terminamos de configurar el servidor y agregamos el archivo mutation y lo importamos en resolverMap

---
+++ src/resolvers/mutation.ts
@@ -0,0 +1,8 @@
+import { IResolvers } from "graphql-tools";
+
+const mutation : IResolvers = {
+    Mutation : {        
+    }
+}
+
+export default mutation;
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,35 @@
-import express from 'express';
-import compression from 'compression';
-import cors from 'cors';
-import schema from './schema';
-import { ApolloServer } from 'apollo-server-express';
-import { createServer } from 'http';
-const app = express();
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+import schema from "./schema";
+import { ApolloServer } from "apollo-server-express";
+import { createServer } from "http";
+import environments from "./config/environments";
 
-app.use('*', cors());
+if (process.env.NODE_ENV !== "production") {
+  const envs = environments;
+  console.log(envs);
+}
 
-app.use(compression());
+async function init() {
+  const app = express();
 
-const server = new ApolloServer({
+  app.use("*", cors());
+
+  app.use(compression());
+
+  const server = new ApolloServer({
     schema,
     introspection: true
-});
+  });
 
-server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-const PORT = 5300;
-const httpServer = createServer(app);
-httpServer.listen(
-    { port : PORT },
-    () => console.log(`Hola Mundo API GraphQL http://localhost:${PORT}/graphql`)
-);
+  const PORT = process.env.PORT || 5300;
+  const httpServer = createServer(app);
+  httpServer.listen({ port: PORT }, () =>
+    console.log(`Hola Mundo API GraphQL http://localhost:${PORT}/graphql`)
+  );
+}
+
+init();
